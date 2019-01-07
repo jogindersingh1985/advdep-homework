4d1a0436b7ef9ff265562c840654e5896d2bc40c
Joginder Singh <jogindersingh1985@gmail.com>
2019-01-07 01:45:56 +0000

    Playbook update 2

--- scripts/pvs_10Gsize.sh
+++ scripts/pvs_10Gsize.sh
@@ -3,4 +3,30 @@
 export GUID=`hostname|awk -F. '{print $2}'`
 export volsize="10Gi"
 
-????
+mkdir /root/pvs
+for volume in pv{1..200} ; do
+cat << EOF > /root/pvs/${volume}
+{
+  "apiVersion": "v1",
+  "kind": "PersistentVolume",
+  "metadata": {
+    "name": "${volume}"
+  },
+  "spec": {
+    "capacity": {
+        "storage": "10Gi"
+    },
+    "accessModes": [ "ReadWriteOnce" ],
+    "nfs": {
+        "path": "/srv/nfs/user-vols/${volume}",
+        "server": "support1.$GUID.internal"
+    },
+    "persistentVolumeReclaimPolicy": "Recycle"
+  }
+}
+EOF
+echo "Created def file for ${volume}";
+done;
+
+cat /root/pvs/* | oc create -f -
+exit 0
