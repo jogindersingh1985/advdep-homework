65c6e4a10ceb007da6322d18787b3a216e337cbf
Joginder Singh <jogindersingh1985@gmail.com>
2019-01-12 11:55:50 +0000

    Added HPA to Tasks-Prod

--- scripts/nodejs_mongo_app.sh
+++ scripts/nodejs_mongo_app.sh
@@ -7,4 +7,6 @@ oc new-app jenkins-persistent --param ENABLE_OAUTH=true --param MEMORY_LIMIT=2Gi
 cd $HOME/advdep-homework/tasks 
 ansible-galaxy install -r requirements.yml --roles-path=galaxy 
 ansible-playbook -i ./.applier/ galaxy/openshift-applier/playbooks/openshift-cluster-seed.yml
+oc create -f tasks-hpa.yaml
 cd -
+
