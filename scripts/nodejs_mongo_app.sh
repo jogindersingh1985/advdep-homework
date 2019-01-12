603a5a451e3b9ff2da07093da90efd251ad8eedf
Joginder Singh <jogindersingh1985@gmail.com>
2019-01-12 05:00:09 +0000

    Updated scripts/nodejs_mongo_app.sh with Build Project

--- scripts/nodejs_mongo_app.sh
+++ scripts/nodejs_mongo_app.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
-cd tasks 
+
+oc new-project tasks-build --display-name="Spring Rest App - Build"
+
+oc new-app jenkins-persistent --param ENABLE_OAUTH=true --param MEMORY_LIMIT=2Gi --param VOLUME_CAPACITY=4Gi --param DISABLE_ADMINISTRATIVE_MONITORS=true -n tasks-build
+
+cd $HOME/advdep-homework/tasks 
 ansible-galaxy install -r requirements.yml --roles-path=galaxy 
 ansible-playbook -i ./.applier/ galaxy/openshift-applier/playbooks/openshift-cluster-seed.yml
 cd -
