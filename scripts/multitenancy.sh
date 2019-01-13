18cae7b77eb0d1d062cfc81ec2876da92e78b904
Joginder Singh <jogindersingh1985@gmail.com>
2019-01-13 12:48:00 +0000

    Added Project Template2

---
+++ scripts/multitenancy.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/sh
+
+export GUID=`hostname | cut -d"." -f2`
+
+ansible masters -m shell -a 'htpasswd -b /etc/origin/master/htpasswd amy r3dh4t1!'
+ansible masters -m shell -a 'htpasswd -b /etc/origin/master/htpasswd andrew r3dh4t1!'
+ansible masters -m shell -a 'htpasswd -b /etc/origin/master/htpasswd brian r3dh4t1!'
+ansible masters -m shell -a 'htpasswd -b /etc/origin/master/htpasswd betty r3dh4t1!'
+
+oc adm groups new alpha amy andrew
+oc adm groups new beta brian betty
+
+for OCP_USERNAME in amy andrew brian betty; do
+
+oc create clusterquota clusterquota-$OCP_USERNAME \
+ --project-annotation-selector=openshift.io/requester=$OCP_USERNAME \
+ --hard pods=30 \
+ --hard requests.memory=5Gi \
+ --hard requests.cpu=5 \
+ --hard limits.cpu=30  \
+ --hard limits.memory=60Gi \
+ --hard configmaps=30 \
+ --hard persistentvolumeclaims=30  \
+ --hard services=30
+
+done
+
+pwd=`pwd`
+export pwd=$pwd/project-template.yaml
+echo "Create template from ${pwd}"
+oc create -f $pwd -n default || oc replace -f $pwd -n default
+
+ansible masters -m shell -a "sed -i 's/projectRequestTemplate.*/projectRequestTemplate\: \"default\/project-request\"/g' /etc/origin/master/master-config.yaml"
+ansible masters -m shell -a'systemctl restart atomic-openshift-node; /usr/local/bin/master-restart api; /usr/local/bin/master-restart controllers'
